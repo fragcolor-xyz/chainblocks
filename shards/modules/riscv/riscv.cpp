e654f405b141c5826c06e8576c3f527010c6e5f0
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-11-06 18:42:10 +0800

    Add Call shard

--- shards/modules/riscv/riscv.cpp
+++ shards/modules/riscv/riscv.cpp
@@ -3,6 +3,7 @@
 #include <shards/common_types.hpp>
 
 #include <libriscv/machine.hpp>
+#include <libriscv/prepared_call.hpp>
 
 namespace shards {
 namespace shriscv {
@@ -109,12 +110,58 @@ struct RiscVSimulate {
   }
 
   SHVar activate(SHContext *shContext, const SHVar &input) {
-    auto machine = varAsObjectChecked<Types::RiscVMachinePtr>(_machine.get(), Types::RiscVMachine);
+    auto &machine = varAsObjectChecked<Types::RiscVMachinePtr>(_machine.get(), Types::RiscVMachine);
     if (_steps.isNone())
       machine->simulate();
     else
       machine->simulate(int32_t(_steps.get().payload.intValue));
     return input;
+  }
+};
+
+struct RiscVCall {
+  static SHTypesInfo inputTypes() { return CoreInfo::AnyType; }
+  static SHTypesInfo outputTypes() { return CoreInfo::AnyType; }
+  static SHOptionalString help() { return SHCCSTR("Calls a function in the RISC-V machine."); }
+
+  PARAM_PARAMVAR(_machine, "Machine", "The RISC-V machine to call the function in.",
+                 {Types::RiscVMachine, Types::RiscVMachineVar});
+  PARAM_VAR(_func, "Function",
+            "The function to call. (has to be exported and with signature int32_t(const SHVar *input, SHVar *output))",
+            {CoreInfo::StringType});
+  PARAM_IMPL(PARAM_IMPL_FOR(_machine), PARAM_IMPL_FOR(_func));
+
+  void warmup(SHContext *context) { PARAM_WARMUP(context); }
+  void cleanup(SHContext *context) {
+    _call.reset();
+    _machinePtr = nullptr;
+    PARAM_CLEANUP(context);
+  }
+
+  PARAM_REQUIRED_VARIABLES();
+  SHTypeInfo compose(SHInstanceData &data) {
+    PARAM_COMPOSE_REQUIRED_VARIABLES(data);
+    return outputTypes().elements[0];
+  }
+
+  std::optional<riscv::PreparedCall<riscv::RISCV64, int32_t(const SHVar *, SHVar *)>> _call;
+  void *_machinePtr{nullptr};
+
+  SHVar activate(SHContext *shContext, const SHVar &input) {
+    auto &machine = varAsObjectChecked<Types::RiscVMachinePtr>(_machine.get(), Types::RiscVMachine);
+    if (_machinePtr != machine.get()) {
+      _machinePtr = machine.get();
+      auto func = SHSTRING_PREFER_SHSTRVIEW(_func);
+      _call = riscv::PreparedCall<riscv::RISCV64, int32_t(const SHVar *, SHVar *)>(*machine, func);
+    }
+
+    SHVar output{};
+    int res = (*_call)(&input, &output);
+    if (res != 0) {
+      throw shards::ActivationError(fmt::format("Failed to call function '{}' error: {}", _func, res));
+    }
+
+    return output;
   }
 };
 
@@ -124,4 +171,5 @@ struct RiscVSimulate {
 SHARDS_REGISTER_FN(shriscv) {
   REGISTER_SHARD("RISC-V.Load", shards::shriscv::RiscVModule);
   REGISTER_SHARD("RISC-V.Simulate", shards::shriscv::RiscVSimulate);
+  REGISTER_SHARD("RISC-V.Call", shards::shriscv::RiscVCall);
 }
