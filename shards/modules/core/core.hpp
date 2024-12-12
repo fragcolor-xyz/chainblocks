6fdb4126230d9f22cae980d2ca4250d7299d1971
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-12-12 16:53:33 +0800

    fix(core): update variable reference logic in warmup

--- shards/modules/core/core.hpp
+++ shards/modules/core/core.hpp
@@ -1280,7 +1280,11 @@ struct Set : public SetUpdateBase {
   }
 
   void warmup(SHContext *context) {
-    SetBase::warmup(context);
+    if (_global)
+      _target = referenceGlobalVariable(context, _name.c_str());
+    else
+      _target = referenceWireVariable(context->currentWire(), _name.c_str());
+    _key.warmup(context);
 
     shassert_extended(context, _self && "Self should be valid at this point");
 
